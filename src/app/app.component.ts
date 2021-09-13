1c475ad00cb8851eec6c2181528b4c033ecf2752
afolabiopakunle <opakunleafolabi@gmail.com>
2021-09-13 11:41:04 +0100

    new test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,28 @@
-import { Component, VERSION } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
   datas: Observable<any>;
   numbers: number = 0;
   
- data = new Observable(observer => {
-    setInterval(() => {
-      observer.next(this.numbers++)
-    }, 2000);
 
-  }).subscribe(result => {
-    // this.numbers++
-    console.log(this.numbers)
-  })
-  
-  // .unsubscribe();
-clicked() {
-  this.data.unsubscribe()
-}
+  ngOnInit() {
+    const personalObservable =  new Observable(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count++);
+      }, 1000)
+    })
+
+    personalObservable.subscribe(data => {
+      console.log(data)
+    })
+  }
 
 
   
